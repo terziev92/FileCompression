7a1e87907bf842ff73e9ba2021af046aae9711d8
Toni <tterziev@qualityhouse.biz>
2015-07-14 15:15:59 +0300

    1.0

---
+++ Compress.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<fstream>
+#include<string>>
+using namespace std;
+
+int main()
+{
+	// Reading input file
+	cout << "Enter a file name to open: " << endl;
+	string filename;
+	cin >> filename;
+	ifstream in_file;
+	in_file.open(filename.c_str());
+	if(in_file.fail())
+	{
+		cout << "Error opening the file" << endl;
+	}
+	else
+	{
+		//executing the program code 2
+		cout << "ok" << endl;
+		in_file.close();
+	}
+	
+	
+	return 0;
+}
